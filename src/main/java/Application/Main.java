c785db74c497c1524fb4ab2a1b078efae0b296ee
rmußler <rmuler@PC-EDV-W05.w-kaechele.local>
2025-05-16 10:05:15 +0200

    Update

--- src/main/java/Application/Main.java
+++ src/main/java/Application/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 		
 		Scene scene = new Scene(new Pane());
 		PageSwitcher.setScene(scene);
-		PageSwitcher.switchTo(Page.FIRST);
+		PageSwitcher.switchTo(Page.MAIN);
 		
 		stage.setTitle("Essen Application");
 		stage.setScene(scene);
