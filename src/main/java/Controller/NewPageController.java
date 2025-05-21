9cf02545e7c3d9701f2e3d0b84c5697da33db1d3
Dieterprofaxe <Robin.mslr11@gmail.com>
2025-05-21 19:10:56 +0200

    update

--- src/main/java/Controller/NewPageController.java
+++ src/main/java/Controller/NewPageController.java
@@ -1,31 +1,49 @@
 package Controller;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import Enums.Page;
 import PageSwitching.PageSwitcher;
-import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-import javafx.scene.control.PasswordField;
 
 public class NewPageController {
 
-	
-	@FXML
-	private Button cancelButton, saveButton;
-	
-	
-	
-	
-	
-	
-	@FXML
-	private void close() {
-		PageSwitcher.switchTo(Page.FIRST);
-	}
-	
-	
-	
+    @FXML
+    private Button cancelButton, saveButton;
+
+    @FXML
+    private VBox zutatenContainer;
+
+    @FXML
+    private VBox zutatenList;
+
+    
+    private int zutatCounter = 3; 
+
+    @FXML
+    private void close() {
+        PageSwitcher.switchTo(Page.FIRST);
+    }
+
+    @FXML
+    private void addZutatRow() {
+        HBox row = new HBox(5);
+        for (int i = 0; i < 2; i++) {
+            TextField tf = new TextField();
+            tf.setPromptText("Zutat " + zutatCounter++);
+            tf.setPrefWidth(150);
+            row.getChildren().add(tf);
+        }
+        zutatenList.getChildren().add(row);
+    }
+    
+    @FXML
+    private void deleteZutatRow() {
+    	
+    }
+    
+    
+    
 }
